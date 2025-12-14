fb127c60ba37861f87598c2b936d966042c9d87f
fsandri-alpenite <fsandri@alpenite.com>
2025-12-14 22:20:09 +0100

    maestra e nuova canzone natalizia

--- app.js
+++ app.js
@@ -40,7 +40,7 @@ const HOUSES_DATA = {
     },
     "4B": {
         "GRIFONDORO": {
-            "students": ["Marco", "Giulia", "Samir", "Luna", "Ibrahim", "Saif Eddine", "Elia", "Manaf", "Afra", "Laura", "Sneha", "Maestro Luca", "Annamaria"],
+            "students": ["Marco", "Giulia", "Samir", "Luna", "Ibrahim", "Saif Eddine", "Elia", "Manaf", "Afra", "Laura", "Sneha", "Maestro Luca", "Annamaria", "Maestra Giorgia"],
             "colors": ["#8B0000", "#DAA520"],
             "description": "Casa del coraggio",
             "traits": "Coraggiosi",
